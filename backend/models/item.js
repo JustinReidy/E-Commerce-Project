bf1ae740647c8dda72e048993fa2416d27eadb26
Justin Reidy <justintreidy@gmail.com>
2026-01-02 09:24:34 -0700

    basic api implementation for Item CRUD

---
+++ backend/models/item.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const itemSchema = new mongoose.Schema({
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    imageAlt: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    colors: {
+        type: Array,
+        required: true,
+    },
+    sizes: {
+        type: Array,
+        required: true,
+    },
+    price: {
+        type: Number,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    dateAdded: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Item', itemSchema)
